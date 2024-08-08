9c5059531495b64fa7eb3da8a4eb93507d8c7d0d
JP Crounse <johnpeter.crounse@netpresenter.com>
2024-08-08 03:56:02 -0800

    Derp

--- ssr-app/src/main.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="@angular/localize" />
-
-import { AppComponent } from './app/app.component';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
